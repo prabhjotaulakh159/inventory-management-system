49639a97fa5ad43b034b32099a757064d088ff55
Prabhjot Aulakh <prabhjot.aulakh@dawsoncollege.qc.ca>
2023-11-25 20:54:40 -0500

    Audit enum

---
+++ src/main/java/prabhjot/safin/retail/services/audit/AuditTable.java
@@ -0,0 +1,19 @@
+package prabhjot.safin.retail.services.audit;
+
+/**
+ * Defines the names of all tables that are audited
+ * @author Prabhjot Aulakh, Safin Haque
+ */
+public enum AuditTable {
+    ADMINS,
+    CUSTOMERS,
+    CATEGORIES,
+    WAREHOUSES,
+    PRODUCTS,
+    PRODUCTS_WAREHOUSES,
+    STORES_AUDIT,
+    PRODUCTS_STORES_AUDIT,
+    ORDERS_AUDIT,
+    ORDERS_PRODUCTS_AUDIT,
+    REVIEWS_AUDIT
+}
