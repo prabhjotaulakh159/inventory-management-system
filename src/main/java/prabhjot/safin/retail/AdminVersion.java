1a5b3c63739df82921f8dc38abf0e8a0671dbdb1
Safin Haque <safinhq13@gmail.com>
2023-11-26 21:36:19 -0500

    Bug fixes on both App Classes

--- src/main/java/prabhjot/safin/retail/AdminVersion.java
+++ src/main/java/prabhjot/safin/retail/AdminVersion.java
@@ -1,10 +1,12 @@
 package prabhjot.safin.retail;
 
+import java.sql.SQLException;
+
 import prabhjot.safin.retail.apps.AdminApp;
 import prabhjot.safin.retail.apps.Application;
 
 public class AdminVersion {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Application admin = new AdminApp();
         admin.run();
     }
--- src/main/java/prabhjot/safin/retail/CustomerVersion.java
+++ src/main/java/prabhjot/safin/retail/CustomerVersion.java
@@ -1,11 +1,15 @@
 package prabhjot.safin.retail;
 
+import java.sql.SQLException;
+
 import prabhjot.safin.retail.apps.Application;
 import prabhjot.safin.retail.apps.CustomerApp;
 
-public class CustomerVersion {
-    public static void main(String[] args) {
+public class CustomerVersion  {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Application customer = new CustomerApp();
         customer.run();
+        
+        
     }
 }
